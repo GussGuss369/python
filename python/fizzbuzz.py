cee051cfe1c49ac221718a754532882954e0818a
GussGuss369 <92728771+GussGuss369@users.noreply.github.com>
2021-10-18 13:42:47 +0100

    Add files via upload

--- python/fizzbuzz.py
+++ python/fizzbuzz.py
@@ -1,10 +1,12 @@
-for num in range(1,101):
-  if num % 3 == 0 and num % 5 == 0:
-    print("FizzBuzz")
-  elif num % 3 == 0:
-    print("Fizz")
-  elif num % 5 == 0:
-    print("Buzz")
-  else:
-    print(num)
-print("\n")
+def FizzBuzz(x):
+  for num in range(1, x + 1):
+    if num % 3 == 0 and num % 5 == 0:
+      print("FizzBuzz")
+    elif num % 3 == 0:
+      print("Fizz")
+    elif num % 5 == 0:
+      print("Buzz")
+    else:
+      print(num)
+user = int(input("enter the range you would like: "))      
+FizzBuzz(user)

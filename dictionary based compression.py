490361b56c7fbea82603f3ca390b80e60e591f0c
GussGuss369 <husseinnahar05@gmail.com>
2022-01-18 12:48:25 +0000

    first task

--- dictionary based compression.py
+++ dictionary based compression.py
@@ -3,10 +3,14 @@ word = input("enter the word you would want to check for repetition: ").lower()
 counter = 0
 nstatement = statement.split()
 index = 0
+if word in statement:
+    for i in range(0, len(nstatement)):
+        index = index + 1
+        if word == nstatement[i]:
+            counter = counter + 1
+            print(f"the word ask in in position {index}")
 
-for i in range(0, len(nstatement)):
-    index = index + 1
-    if word == nstatement[i]:
-        counter = counter + 1
-        print(f"the word ask in in position {index}")
+else:
+    print("Error. The word is not in the sentece")
+
 print(f"the word {word} repeats {counter} time(s)")
